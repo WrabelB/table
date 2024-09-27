d260e0e65ceaf564cbf97554f744b32f62d8b9c4
Bencsik Botond <bencsik.botond@BOLYAI.LOCAL>
2024-09-27 09:48:02 +0200

    JavaScript

---
+++ main.js
@@ -0,0 +1,24 @@
+const array = [
+    {
+        firstname1: 'Géza',
+        firstname2: 'Ferenc',
+        lastname: 'Kocsis'
+    },
+    {
+        firstname1: 'Mária',
+        firstname2: 'Júlia',
+        lastname: 'Horváth'
+    },
+    {
+        firstname1: 'Ferenc',
+        lastname: 'Balogh'
+    },
+    {
+        firstname1: 'Gábor',
+        firstname2: 'Attila',
+        lastname: 'Horváth'
+    },
+]
+
+const table = document.createElement("table");
+document.body.appendChild(table)
