2ad0dbbed5d3386499f074ed313dafd4d9a200a9
Bencsik Botond <bencsik.botond@BOLYAI.LOCAL>
2024-10-04 09:34:21 +0200

    Hazas es Allatok

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
 
-const array = [
+let array = [
     {
         firstname1: 'Géza',
         firstname2: 'Ferenc',
-        lastname: 'Kocsis'
+        lastname: 'Kocsis',
+        married: true,
+        pet: 'kutya'
     },
     {
         firstname1: 'Mária',
         firstname2: 'Júlia',
-        lastname: 'Horváth'
+        lastname: 'Horváth',
+        married: false,
+        pet: 'macska'
     },
     {
         firstname1: 'Ferenc',
-        lastname: 'Balogh'
+        lastname: 'Balogh',
+        married: false,
+        pet: 'teknős'
     },
     {
         firstname1: 'Gábor',
         firstname2: 'Attila',
-        lastname: 'Horváth'
+        lastname: 'Horváth',
+        married: true,
+        pet: 'macska'
     },
 ]
 const table = document.createElement('table');
@@ -25,42 +33,62 @@ const tableheader = document.createElement('thead');
 table.appendChild(tableheader);
 const tableheaderRow = document.createElement('tr');
 tableheader.appendChild(tableheaderRow);
+
 const lastname = document.createElement('th');
 lastname.innerHTML = "Vezetéknév";
 const firstname = document.createElement('th');
 firstname.innerHTML = "Keresztnév";
-tableheaderRow.appendChild(firstname);
-tableheaderRow.appendChild(lastname);
+const married = document.createElement('th');
+married.innerHTML = "Házasság";
+const pet = document.createElement('th');
+pet.innerHTML = "Állat";
+
 document.body.appendChild(table);
 const tbody = document.createElement('tbody');
 table.appendChild(tbody);
 
+tableheaderRow.appendChild(firstname);
+tableheaderRow.appendChild(lastname);
+tableheaderRow.appendChild(married);
+tableheaderRow.appendChild(pet);
+
+
 for (person of array) {
     const row = document.createElement('tr');
     const fn = document.createElement('td');
+    const marriedTableBodyRow = document.createElement('td');
+    const petTableBodyRow = document.createElement('td');
+
     fn.innerHTML = person.firstname1
     if (person.firstname2){
         fn.innerHTML += " " + person.firstname2;
     }
     const ln = document.createElement('td');
     ln.innerHTML = person.lastname;
+
+    marriedTableBodyRow.innerHTML = person.married? 'Igen':'Nem';
+    petTableBodyRow.innerHTML = person.pet;
+
+
     row.appendChild(ln);
     row.appendChild(fn);
     tbody.appendChild(row);
+    row.appendChild(marriedTableBodyRow);
+    row.appendChild(petTableBodyRow);
+    
 
-
+    
     row.addEventListener('click', function(e)
     {
-        let a = tbody.querySelector('.selected');
+        let professzionalisValtozoAmitHasznalokUgyseTalalodKiMireHasznalomHahaha = tbody.querySelector('.selected');
         console.log("clicked:" + lastname);
         
-        if(a){
-            a.classList.remove('selected');
-        }
+        if(professzionalisValtozoAmitHasznalokUgyseTalalodKiMireHasznalomHahaha)
+            professzionalisValtozoAmitHasznalokUgyseTalalodKiMireHasznalomHahaha.classList.remove('selected');
+
         e.currentTarget.classList.add('selected');
         
     })
-   
-        
+
 }
 
