e3a48836a9a3948cc9e4fe0d3263c03e164586f2
Bencsik Botond <bencsik.botond@bolyai.local>
2024-10-04 08:08:25 +0200

    JavaScript_Marhasag

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 const array = [
     {
         firstname1: 'Géza',
@@ -19,6 +20,31 @@ const array = [
         lastname: 'Horváth'
     },
 ]
+const table = document.createElement('table');
+const tableheader = document.createElement('thead');
+table.appendChild(tableheader);
+const tableheaderRow = document.createElement('tr');
+tableheader.appendChild(tableheaderRow);
+const lastname = document.createElement('th');
+lastname.innerHTML = "Vezetéknév";
+const firstname = document.createElement('th');
+firstname.innerHTML = "Keresztnév";
+tableheaderRow.appendChild(firstname);
+tableheaderRow.appendChild(lastname);
+document.body.appendChild(table);
+const tbody = document.createElement('tbody');
+table.appendChild(tbody);
 
-const table = document.createElement("table");
-document.body.appendChild(table)
+for (person of array) {
+    const row = document.createElement('tr');
+    const fn = document.createElement('td');
+    fn.innerHTML = person.firstname1
+    if (person.firstname2){
+        fn.innerHTML += " " + person.firstname2;
+    }
+    const ln = document.createElement('td');
+    ln.innerHTML = person.lastname;
+    row.appendChild(ln);
+    row.appendChild(fn);
+    tbody.appendChild(row);
+}
