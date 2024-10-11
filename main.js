0a8f88924b09b09456b09bedf7d4bbb54a7f1248
Bencsik Botond <bencsik.botond@BOLYAI.LOCAL>
2024-10-11 08:50:30 +0200

    Table Person valami

--- main.js
+++ main.js
@@ -47,54 +47,18 @@ document.body.appendChild(table);
 const tbody = document.createElement('tbody');
 table.appendChild(tbody);
 
-tableheaderRow.appendChild(firstname);
 tableheaderRow.appendChild(lastname);
+tableheaderRow.appendChild(firstname);
 tableheaderRow.appendChild(married);
 tableheaderRow.appendChild(pet);
 
 
-for (person of array) {
-    const row = document.createElement('tr');
-    const fn = document.createElement('td');
-    const marriedTableBodyRow = document.createElement('td');
-    const petTableBodyRow = document.createElement('td');
-
-    fn.innerHTML = person.firstname1
-    if (person.firstname2){
-        fn.innerHTML += " " + person.firstname2;
-    }
-    const ln = document.createElement('td');
-    ln.innerHTML = person.lastname;
-
-    marriedTableBodyRow.innerHTML = person.married? 'Igen':'Nem';
-    petTableBodyRow.innerHTML = person.pet;
-
-
-    row.appendChild(ln);
-    row.appendChild(fn);
-    tbody.appendChild(row);
-    row.appendChild(marriedTableBodyRow);
-    row.appendChild(petTableBodyRow);
-    
-
-    
-    row.addEventListener('click', function(e)
-    {
-        let professzionalisValtozoAmitHasznalokUgyseTalalodKiMireHasznalomHahaha = tbody.querySelector('.selected');
-        console.log("clicked:" + lastname);
-        
-        if(professzionalisValtozoAmitHasznalokUgyseTalalodKiMireHasznalomHahaha)
-            professzionalisValtozoAmitHasznalokUgyseTalalodKiMireHasznalomHahaha.classList.remove('selected');
-
-        e.currentTarget.classList.add('selected');
-        
-    });
-
-};
 
 const form = document.getElementById('form');
 form.addEventListener('submit', function(e)
+    
 {
+    tbody.innerHTML = ''
     e.preventDefault();
     const lastname = document.getElementById('lastname');
     const firstname1 = document.getElementById('firstname1');
@@ -105,7 +69,7 @@ form.addEventListener('submit', function(e)
     const LastnameVale = lastname.value;
     const FirstnameValue1 = firstname1.value;
     const FirstnameValue2 = firstname2.value;
-    const MarriedValue = married.value;
+    const MarriedValue = married.checked;
     const PetValue = pet.value;
 
     array.push(
@@ -114,7 +78,53 @@ form.addEventListener('submit', function(e)
             firstname2: FirstnameValue2, married: MarriedValue,
             pet: PetValue
         });
-    console.log('array');
+    console.log(array);
+    RendelTéböl();
 });
 
+RendelTéböl();
 
+function RendelTéböl()
+{
+    for (person of array) {
+        const row = document.createElement('tr');
+        const fn = document.createElement('td');
+        const fn1 = document.createElement('td');
+        const marriedTableBodyRow = document.createElement('td');
+        const petTableBodyRow = document.createElement('td');
+    
+        fn.innerHTML = person.firstname1
+        fn1.innerHTML = person.firstname2
+        if (person.firstname2){
+            fn.innerHTML += " " + person.firstname2;
+        }
+        const ln = document.createElement('td');
+        ln.innerHTML = person.lastname;
+    
+        marriedTableBodyRow.innerHTML = person.married? 'Igen':'Nem';
+        petTableBodyRow.innerHTML = person.pet;
+    
+    
+        row.appendChild(ln);
+        row.appendChild(fn);
+        tbody.appendChild(row);
+        row.appendChild(marriedTableBodyRow);
+        row.appendChild(petTableBodyRow);
+        
+    
+        
+        row.addEventListener('click', function(e)
+        {
+            let professzionalisValtozoAmitHasznalokUgyseTalalodKiMireHasznalomHahaha = tbody.querySelector('.selected');
+            console.log("clicked:" + lastname);
+            
+            if(professzionalisValtozoAmitHasznalokUgyseTalalodKiMireHasznalomHahaha)
+                professzionalisValtozoAmitHasznalokUgyseTalalodKiMireHasznalomHahaha.classList.remove('selected');
+    
+            e.currentTarget.classList.add('selected');
+            
+        });
+    
+    };
+
+}
