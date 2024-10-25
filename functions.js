a1f3c7a7508099ae67050d3d02ba8d165f35877d
Bencsik Botond <bencsik.botond@BOLYAI.LOCAL>
2024-10-25 09:24:53 +0200

    Refaktorizalas

---
+++ functions.js
@@ -0,0 +1,113 @@
+/**
+ * 
+ * @param {'td'| 'th'} tagName 
+ * @param {string} innerHTML 
+ * @param {HTMLTableRowElement} parent 
+ */
+function CreateTableCell(tagName, innerHTML, parent)
+{
+    const cella = document.createElement(tagName);
+    cella.innerHTML = innerHTML;
+    parent.appendChild(cella);
+    return cella;
+}
+
+
+/**
+ * 
+ * @param {*} tag 
+ * @param {number} id
+ * @param {HTMLElement} parent
+ */
+function createHTMLElement(tag, id, parent)
+{
+    const tag_HTML = document.createElement(tag);
+    tag_HTML.id = id;
+    parent.appendChild(tag_HTML)
+}
+
+function createHTMLElementWithParentID(tag, id, parentid)
+{
+    const parent = document.getElementById(parentid);
+    if(parent != undefined)
+       createHTMLElement(tag, id, parent);
+}
+
+function RendelTéböl()
+{
+    for (person of array) {
+        const row = document.createElement('tr');
+        tbody.appendChild(row);
+        const fn = document.createElement('td');
+        const fn1 = document.createElement('td');
+        const marriedTableBodyRow = document.createElement('td');
+        const petTableBodyRow = document.createElement('td');
+    
+        fn.innerHTML = person.firstname1
+        fn1.innerHTML = person.firstname2
+        if (person.firstname2){
+            fn.innerHTML += " " + person.firstname2;
+        }
+        const ln = document.createElement('td');
+        ln.innerHTML = person.lastname;
+    
+        marriedTableBodyRow.innerHTML = person.married? 'Igen':'Nem';
+        petTableBodyRow.innerHTML = person.pet;
+    
+    
+        row.appendChild(ln);
+        row.appendChild(fn);
+        row.appendChild(marriedTableBodyRow);
+        row.appendChild(petTableBodyRow);
+       
+        
+        row.addEventListener('click', function(e)
+        {
+            let professzionalisValtozoAmitHasznalokUgyseTalalodKiMireHasznalomHahaha = tbody.querySelector('.selected');
+            console.log("clicked:" + lastname);
+            
+            if(professzionalisValtozoAmitHasznalokUgyseTalalodKiMireHasznalomHahaha)
+                professzionalisValtozoAmitHasznalokUgyseTalalodKiMireHasznalomHahaha.classList.remove('selected');
+    
+            e.currentTarget.classList.add('selected');
+            
+        });
+    
+    };
+
+}
+
+
+function ValidétFíldsz(lastname, firstname1, pet)
+{
+    const errorMessages = form.querySelectorAll('.error');
+    for(const error of errorMessages)
+    {
+        error.innerHTML = '';
+    }
+    let result = true;
+    if(lastname.value === '')
+    {
+        let error = lastname.parentElement.querySelector('.error');
+        error.innerHTML = "A vezeteknev kotelezo :D"
+        result = false;
+    }
+    if(firstname1.value === '')
+    {
+        let error = firstname1.parentElement.querySelector('.error');
+        error.innerHTML = "A firstname kotelezo, IRD MAR ODA EMBER!!!."
+        result = false;
+    }
+    if(pet.value === '')
+    {
+        let error = pet.parentElement.querySelector('.error');
+        error.innerHTML = "A pet kotelezo, tudom hogy van allatod, latlak."
+        result = false;
+    }    
+    return result;
+
+}
+
+
+
+// Hazi: Functions.js osszes parameteret a csillagos cuccba 
--- main.js
+++ main.js
@@ -28,17 +28,17 @@ let array = [
         pet: 'macska'
     },
 ]
-const table = document.createElement('table');
-const tableheader = document.createElement('thead');
-table.appendChild(tableheader);
-const tableheaderRow = document.createElement('tr');
-tableheader.appendChild(tableheaderRow);
+
+createHTMLElement('table', 'persontable', document.body);
+createHTMLElementWithParentID('thead', 'personthead', 'persontable');
+createHTMLElementWithParentID('tr', 'persontr', 'personthead');
 
 
-CreateTableCell('th', 'Vezetéknév', tableheaderRow);
-CreateTableCell('th', 'Keresztnév', tableheaderRow);
-CreateTableCell('th', 'Házasság', tableheaderRow);
-CreateTableCell('th', 'Állat', tableheaderRow);
+
+CreateTableCell('th', 'Vezetéknév', persontr);
+CreateTableCell('th', 'Keresztnév', persontr)
+CreateTableCell('th', 'Házasság', persontr);
+CreateTableCell('th', 'Állat', persontr);
 
 document.body.appendChild(table);
 const tbody = document.createElement('tbody');
@@ -82,93 +82,3 @@ form.addEventListener('submit', function(e)
 });
 
 RendelTéböl();
-
-function RendelTéböl()
-{
-    for (person of array) {
-        const row = document.createElement('tr');
-        tbody.appendChild(row);
-        const fn = document.createElement('td');
-        const fn1 = document.createElement('td');
-        const marriedTableBodyRow = document.createElement('td');
-        const petTableBodyRow = document.createElement('td');
-    
-        fn.innerHTML = person.firstname1
-        fn1.innerHTML = person.firstname2
-        if (person.firstname2){
-            fn.innerHTML += " " + person.firstname2;
-        }
-        const ln = document.createElement('td');
-        ln.innerHTML = person.lastname;
-    
-        marriedTableBodyRow.innerHTML = person.married? 'Igen':'Nem';
-        petTableBodyRow.innerHTML = person.pet;
-    
-    
-        row.appendChild(ln);
-        row.appendChild(fn);
-        row.appendChild(marriedTableBodyRow);
-        row.appendChild(petTableBodyRow);
-        
-    
-        
-        row.addEventListener('click', function(e)
-        {
-            let professzionalisValtozoAmitHasznalokUgyseTalalodKiMireHasznalomHahaha = tbody.querySelector('.selected');
-            console.log("clicked:" + lastname);
-            
-            if(professzionalisValtozoAmitHasznalokUgyseTalalodKiMireHasznalomHahaha)
-                professzionalisValtozoAmitHasznalokUgyseTalalodKiMireHasznalomHahaha.classList.remove('selected');
-    
-            e.currentTarget.classList.add('selected');
-            
-        });
-    
-    };
-
-}
-/**
- * 
- * @param {'td'| 'th'} tagName 
- * @param {string} innerHTML 
- * @param {HTMLTableRowElement} parent 
- */
-function CreateTableCell(tagName, innerHTML, parent)
-{
-    const cella = document.createElement(tagName);
-    cella.innerHTML = innerHTML;
-    parent.appendChild(cella);
-    return cella;
-}
-
-
-
-function ValidétFíldsz(lastname, firstname1, pet)
-{
-    const errorMessages = form.querySelectorAll('.error');
-    for(const error of errorMessages)
-    {
-        error.innerHTML = '';
-    }
-    let result = true;
-    if(lastname.value === '')
-    {
-        let error = lastname.parentElement.querySelector('.error');
-        error.innerHTML = "A vezeteknev kotelezo :D"
-        result = false;
-    }
-    if(firstname1.value === '')
-    {
-        let error = firstname1.parentElement.querySelector('.error');
-        error.innerHTML = "A firstname kotelezo, IRD MAR ODA EMBER!!!."
-        result = false;
-    }
-    if(pet.value === '')
-    {
-        let error = pet.parentElement.querySelector('.error');
-        error.innerHTML = "A pet kotelezo, tudom hogy van allatod, latlak."
-        result = false;
-    }    
-    return result;
-
-}
